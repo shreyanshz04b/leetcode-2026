3fe760371d1c9eb8afedb109e46ae5ff564279be
Shreyansh Agarwal <124380884+shreyanshz04b@users.noreply.github.com>
2026-03-24 11:32:09 +0530

    Implement constructProductMatrix method

---
+++ ConstructProductMarix.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[][] constructProductMatrix(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int MOD = 12345;
+        int size = n * m;
+        int[] arr = new int[size];
+        int idx = 0;
+        for (int[] row : grid) {
+            for (int val : row) {
+                arr[idx++] = val % MOD;
+            }
+        }
+        int[] prefix = new int[size];
+        prefix[0] = 1;
+        for (int i = 1; i < size; i++) {
+            prefix[i] = (prefix[i - 1] * arr[i - 1]) % MOD;
+        }
+        int[] suffix = new int[size];
+        suffix[size - 1] = 1;
+        for (int i = size - 2; i >= 0; i--) {
+            suffix[i] = (suffix[i + 1] * arr[i + 1]) % MOD;
+        }
+        int[][] result = new int[n][m];
+        idx = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                result[i][j] = (prefix[idx] * suffix[idx]) % MOD;
+                idx++;
+            }
+        }
+        return result;
+    }
+}
